49c018c40faab5fc20449ffd86fff4bddee5a652
hayato-hamabe <1122hamabe.h@gmail.com>
2022-07-01 20:59:24 +0900

    各画面を読み込んだとき、ロードのクルクルを表示させるようにした

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// ロードのクルクル
+const loader = document.getElementById("js-loader");
+window.addEventListener("load", () => {
+  const ms = 400;
+  loader.style.transition = "opacity " + ms + "ms";
+  const loaderOpacity = function () {
+    loader.style.opacity = 0;
+  };
+  const loaderDisplay = function () {
+    loader.style.display = "none";
+  };
+  setTimeout(loaderOpacity, 1);
+  setTimeout(loaderDisplay, ms);
+});
+
 //low 960px header-btn
 toggle = () => {
   const line = document.getElementById("hamburger-line");
